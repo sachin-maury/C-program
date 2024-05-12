02ad08bb5805bb7b67fc98edd572df0e4005c5aa
Sachin Maurya <kumarsachin67671@gmail.com>
2024-05-13 00:19:22 +0530

    increment operator

---
+++ practise8.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+ int a = 21;
+ int c ;
+ c = a;
+ printf("c = %d\n", c );
+ c += a;
+ printf("Value of c = %d\n", c );
+ c -= a;
+ printf("Value of c = %d\n", c );
+ c *= a;
+ printf("Value of c = %d\n", c );
+}
